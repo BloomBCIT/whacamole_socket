77e45445ea0b5d3ae116195747d0b399c249b5eb
SE HEE AHN <seheeahn@ses-macbook-pro.wifi.bcit.ca>
2018-03-17 16:29:42 -0700

    1213
    2313

--- index.js
+++ index.js
@@ -3,46 +3,71 @@ const server= require("http").Server();
 
 var io = require("socket.io")(server);
 
+//var allusersRoom1 = [];
+//var allusersRoom2 = [];
+
+//test
+
 var allRooms ={};
+var allstickers = {};
+
 
 io.on("connection", function(socket){
+    console.log("someone is connected");
+    
+    
+    //allusers.push(socket.id);
+    
+     socket.on("sticker", function(data){
+        allstickers[this.myRoom].push(data);
+         
+        io.to(this.myRoom).emit("newsticker", allstickers[this.myRoom]);
+        
+    });
     
     socket.on("joinroom", function(data){
+        socket.emit("yourid", socket.id);
         socket.join(data);
-        socket.myRoom = data;
+        //io.emit("createimage", allusers);
+        socket.myRoom =data;
         
         if(!allRooms[data]){
-            allRooms[data] ={
-                users:[],
-                q:{}
-            
-            };
+            allRooms[data] = [];
             
         }
-        console.log(data, "joinroom");
-    });
-    
-    socket.on("qsubmit", function(data){
-        //tell everybody there's a new question
+        
+        if(!allstickers[data]){
+            allstickers[data] = [];
+        }
+        
+        
+        allRooms[data].push(socket.id);
+        io.to(data).emit("createimage", allRooms[data]);
+        
+//        if(data=="room1"){
+//            allusersRoom1.push(socket.id);
+//        io.to(data).emit("createimage", allusersRoom1);
+//            }else if(data=="room2"){
+//                allusersRoom2.push(socket.id);
+//                io.to(data).emit("createimage", allusersRoom2);
+//                
+//            }
         console.log(data);
-        allRooms[socket.myRoom].q =data;
-        socket.to(socket.myRoom).emit("newq", data);   
-        
         
     });
     
-    socket.on("answer", function(data){
-              var msg = "wrong!"
-              if(allRooms[socket.myRoom].q.a == data){
-                    msg = "you got it!";
-              
-              }
-              
-                socket.emit("result",msg)
-              });
+    //console.log(allusers);
+    
+    socket.on("mymove", function(data){
+        socket.to(this.myRoom).emit("usermove", data); 
+    });
+    
+   
     
     socket.on("disconnect", function(){
-        
+        var index = allRooms[this.myRoom].indexOf(socket.id);
+        allRooms[this.myRoom].splice(index, 1);
+        io.to(this.myRoom).emit("createimage", allRooms[this.myRoom]);
     });
 });
 
